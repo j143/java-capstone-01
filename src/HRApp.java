e4e41123e5fcf2b408498573fc7da87f70f7c531
Janardhan Pulivarthi <j143@protonmail.com>
2022-01-20 21:38:12 +0530

    check for multiple inputs

--- src/HRApp.java
+++ src/HRApp.java
@@ -2,7 +2,9 @@ public class HRApp {
     public static void main(String [] args) {
         System.out.println("test harness");
         Employee Jason = new Employee(21, "Jason", 20000.02);
+        Employee Clarice = new Employee(23, "Clarice", 23000.10);
         System.out.println(Jason.toString());
+        System.out.println(Clarice.toString());
 
     }
 }
