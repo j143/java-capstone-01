1ae2256d3b56d5fa4a2315caf4fb39167ec94af0
Janardhan Pulivarthi <j143@protonmail.com>
2022-01-20 20:54:18 +0530

    add fields and default constructor in Employee

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,19 @@
 public class Employee {
+    public static int id;
+    public static String name;
+    public static double salary;
 
+    public static void Employee() {
+        //default constructor
+    };
+
+    public void setEmployee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public void getEmployee(int id) {
+        //
+    }
 }
